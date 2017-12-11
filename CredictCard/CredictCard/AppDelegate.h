b88a265d37c2c4e089588f42d63e1ae7b2952c53
hehong <616248127@qq.com>
2017-12-11 15:47:23 +0800

    12.11
    12.11

---
+++ CredictCard/CredictCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CredictCard
+//
+//  Created by User on 2017/12/11.
+//  Copyright © 2017年 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
